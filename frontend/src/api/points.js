fdcc2eaf3062bc75663460dcf341064f1050bb45
Gabriel <geonelson06@gmail.com>
2026-07-09 20:46:55 -0400

    backend updates

--- frontend/src/api/points.js
+++
@@ -1,5 +0,0 @@
-import { apiClient } from '../lib/apiClient'
-
-export function getUserPoints(userId) {
-  return apiClient.get(`/users/${userId}/points`).then((res) => res.data)
-}
--- frontend/src/api/sections.js
+++
@@ -1,9 +0,0 @@
-import { apiClient } from '../lib/apiClient'
-
-export function listSections() {
-  return apiClient.get('/sections').then((res) => res.data)
-}
-
-export function getSection(sectionId) {
-  return apiClient.get(`/sections/${sectionId}`).then((res) => res.data)
-}
